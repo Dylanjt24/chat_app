dd852387da619febe0b9ee2b579193bcbd6a7e93
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-06-13 14:47:18 -0700

    Created form buttons, onSubmit function, and createNewId function for login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,16 +1,28 @@
 import React, { useRef } from 'react';
-import { Container, Form } from 'react-bootstrap';
+import { Button, Container, Form } from 'react-bootstrap';
+import { v4 as uuidV4 } from 'uuid';
 
-export default function Login() {
+export default function Login({ onIdSubmit }) {
     const idRef = useRef();
 
+    function handleSubmit(e) {
+        e.preventDefault();
+        onIdSubmit(idRef.current.value);
+    }
+
+    function createNewId() {
+        onIdSubmit(uuidV4());
+    }
+
     return (
-    <Container>
-        <Form>
+    <Container className="align-items-center d-flex" style={{height: "100vh"}}>
+        <Form onSubmit={handleSubmit} className="w-100">
             <Form.Group>
                 <Form.Label>Enter Your Id</Form.Label>
-                <Form.Control type="text" ref={idRef} required></Form.Control>
+                <Form.Control type="text" ref={idRef} required />
             </Form.Group>
+            <Button type="submit" className="me-2 mt-3">Login</Button>
+            <Button onClick={createNewId} variant="secondary" className="mt-3">Create A New Id</Button>
         </Form>
     </Container>
     )
