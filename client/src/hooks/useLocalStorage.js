ed10e0464f65ae7815cc95c3131fba5493e01818
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2022-06-13 15:08:36 -0700

    Created useLocalStorage hook for getting key value from local storage

---
+++ client/src/hooks/useLocalStorage.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+
+const PREFIX = "chat-app-"
+
+export default function useLocalStorage(key, initialValue) {
+    const prefixedKey = PREFIX + key;
+    const [value, setValue] = useState(() => { // gets value from localStorage and puts it into state
+        const jsonValue = localStorage.getItem(prefixedKey);
+        if (jsonValue != null) return JSON.parse(jsonValue)
+        if (typeof initialValue === 'function') {
+            return initialValue();
+        } else {
+            return initialValue;
+        }
+    })
+}
